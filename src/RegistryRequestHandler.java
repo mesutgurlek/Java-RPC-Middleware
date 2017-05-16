e42e685dc9f3cbf04b154596473219c5781f17d1
çağrı Kaymak <cagrikymk@gmail.com>
2017-05-16 19:54:57 +0300

    lookup and run method of handler

--- src/RegistryRequestHandler.java
+++ src/RegistryRequestHandler.java
@@ -1,18 +1,22 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
 
 public class RegistryRequestHandler extends Thread {
     private Socket inSocket;
-
+    private HashMap<String, RemoteObjectReference> table;
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
     public RegistryRequestHandler(Socket inSocket) {
+        table = Registry.getLookupTable();
         this.inSocket = inSocket;
     }
 
     public synchronized RemoteObjectReference bind(RegistryMessage message) {
         InetAddress clientAddr = inSocket.getInetAddress();
         RemoteObjectReference remoteObjectReference = new RemoteObjectReference(message, clientAddr);
-        HashMap<String, RemoteObjectReference> table = Registry.getLookupTable();
         String name = message.getName();
         // check the name is unique or not
         if (table.containsKey(name)) {
@@ -27,7 +31,55 @@ public class RegistryRequestHandler extends Thread {
     }
 
     public synchronized RemoteObjectReference lookup(RegistryMessage message) {
+        String name = message.getName();
+        RemoteObjectReference remoteObjectReference;
+        if(table.containsKey(name)) {
+            remoteObjectReference = table.get(name);
+        }
+        else {
+            System.out.println("Object:" + name + " is not in the registry!");
+            remoteObjectReference = null;
+        }
 
+        return remoteObjectReference;
+    }
+
+    public synchronized void listAllObjects() {
+        System.out.println(table.toString());
+    }
+
+    public void run() {
+        try {
+            inputStream = new ObjectInputStream(inSocket.getInputStream());
+            outputStream = new ObjectOutputStream(inSocket.getOutputStream());
+
+            RegistryMessage message = (RegistryMessage) inputStream.readObject();
+
+            switch (message.getType()) {
+                case REBIND:
+                    RemoteObjectReference remoteObjectReference = bind(message);
+                    outputStream.writeObject(remoteObjectReference);
+                    break;
+                case LOOKUP:
+                    RemoteObjectReference remoteObjectReference1 = lookup(message);
+                    outputStream.writeObject(remoteObjectReference1);
+                    break;
+                default:
+                    System.out.println("Invalid request!");
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                inputStream.close();
+                outputStream.close();
+                inSocket.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
