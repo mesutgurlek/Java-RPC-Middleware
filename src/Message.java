3bcefea2c24a83b1a48ce14a2ef95936ae313f44
Enes Akdogan <aeakdogan@gmail.com>
2017-05-16 22:22:16 +0300

    Server side created

---
+++ src/Message.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+
+/**
+ * Created by aeakdogan on 16/05/2017.
+ * ${CLASS}
+ */
+public class Message implements Serializable {
+}
---
+++ src/Server.java
@@ -0,0 +1,10 @@
+
+/**
+ * Created by aeakdogan on 16/05/2017.
+ * ${CLASS}
+ */
+public class Server {
+    public static void main(String[] args){
+        Object object = Naming.bind(new Object(), "object", "localhost", 6000);
+    }
+}
