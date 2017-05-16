7bb983f17b36b8b9099424b35ba4c2e0cfc06030
Enes Akdogan <aeakdogan@gmail.com>
2017-05-16 22:40:34 +0300

    messages initialized

--- src/Message.java
+++ src/Message.java
@@ -1,8 +1,54 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  * Created by aeakdogan on 16/05/2017.
  * ${CLASS}
  */
 public class Message implements Serializable {
+    int id;
+    MessageType messageType;
+    RemoteObjectReference remoteObjectReference;
+    String methodName;
+    ArrayList<Object> arguments;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public RemoteObjectReference getRemoteObjectReference() {
+        return remoteObjectReference;
+    }
+
+    public void setRemoteObjectReference(RemoteObjectReference remoteObjectReference) {
+        this.remoteObjectReference = remoteObjectReference;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public ArrayList<Object> getArguments() {
+        return arguments;
+    }
+
+    public void setArguments(ArrayList<Object> arguments) {
+        this.arguments = arguments;
+    }
 }
