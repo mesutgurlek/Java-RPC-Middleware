4ca0da0d43b104298873911dbd00eb7dce9eb214
Enes Akdogan <aeakdogan@gmail.com>
2017-05-17 19:09:28 +0300

    RemoteReferenceModuleServer added, Dispatcher configured

---
+++ src/RemoteReferenceModuleServer.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+/**
+ * Created by aeakdogan on 17/05/2017.
+ * ${CLASS}
+ */
+public class RemoteReferenceModuleServer {
+    private static RemoteReferenceModuleServer remoteReferenceModuleServer;
+    private HashMap<RemoteObjectReference, Object> remoteObjectReferenceObjectHashMap;
+
+    //SINGLETON PATTERN
+    public static synchronized RemoteReferenceModuleServer getServerRemoteReference(){
+        if(remoteReferenceModuleServer != null)
+            return remoteReferenceModuleServer;
+        return remoteReferenceModuleServer = new RemoteReferenceModuleServer();
+    }
+
+    public RemoteReferenceModuleServer() {
+        remoteObjectReferenceObjectHashMap = new HashMap<>();
+    }
+
+
+    public synchronized void addObjectReference(RemoteObjectReference remoteObjectReference, Object object){
+        remoteObjectReferenceObjectHashMap.put(remoteObjectReference, object);
+    }
+
+    public synchronized void removeObjectReference(RemoteObjectReference remoteObjectReference){
+        remoteObjectReferenceObjectHashMap.remove(remoteObjectReference);
+    }
+
+    public synchronized Object getObjectReference(RemoteObjectReference remoteObjectReference){
+        return remoteObjectReferenceObjectHashMap.get(remoteObjectReference);
+    }
+
+}
